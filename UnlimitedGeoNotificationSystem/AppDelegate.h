9b83b7a99f49384319c8eca43c40cbecd58c621a
joshua <josh@joshuas-imac.local>
2016-06-09 02:09:12 -0700

    Initial Commit

---
+++ UnlimitedGeoNotificationSystem/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  UnlimitedGeoNotificationSystem
+//
+//  Created by joshua on 6/9/16.
+//  Copyright © 2016 joshua. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ UnlimitedGeoNotificationSystem/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  UnlimitedGeoNotificationSystem
+//
+//  Created by joshua on 6/9/16.
+//  Copyright © 2016 joshua. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
